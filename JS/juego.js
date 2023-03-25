82168268379ca4505ab2e4759158ed49dfb1fd8c
Workap <soyh629@gmail.com>
2023-03-24 20:12:04 -0500

    Letras usadas listo/-Contador de vida y de racha (solo visual)

--- JS/juego.js
+++ JS/juego.js
@@ -14,22 +14,12 @@ let random = Math.round(Math.random() * palabras.length);
 let write = palabras[random];
 
 for (let i = 0; i < write.length; i++) {
-  let letra = write[i];
   const letras = document.querySelector(".letras");
   const div = document.createElement("div");
   if (write[i] == " ") {
-    letras.appendChild(div).className = "space";
+    // letras.appendChild(div).className = "space";
+    letras.appendChild(div).className = "letras_chart".innerText = "-";
   } else {
     letras.appendChild(div).className = "letras_chart";
   }
 }
-
-// console.log(letras);
-// console.log(write);
-
-// for (let i = 0; i < write.length; i++) {
-//   console.log(write[i]);
-//   let letra = write[i];
-//   para.innerText = letra;
-//   let para = document.createElement("div");
-// }
