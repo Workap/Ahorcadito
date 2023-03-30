68259793d7514053cac43990120ad1513c69d63e
Workap <soyh629@gmail.com>
2023-03-29 20:30:49 -0500

    Vida echa/-Mensajes de victoria y perdidad solo lo funcional/-Mostrar las letras usas, ya terminado/-Validacion del input text para movil echo/-Vercion movil a un 80%

--- JS/juego.js
+++ JS/juego.js
@@ -11,51 +11,79 @@ let palabras = [
 
 let random = Math.round(Math.random() * palabras.length);
 
-let write = palabras[random];
+let letra = palabras[random];
 
-// for (let i = 0; i < write.length; i++) {
-//   const letras = document.querySelector(".letras");
-//   const div = document.createElement("div");
-//   if (write[i] == " ") {
-//     letras.appendChild(div).className = "space";
-//   } else {
-//     letras.appendChild(div).className = "letras_chart";
-//   }
-// }
-
-// const btnMenu = document.querySelector(".btn__menu");
-
-// btnMenu.addEventListener("click", () => {
-//   alert("Se a clickeado el menu");
-// });
+let vidas = 6;
 
 const form = document.querySelector(".form");
 const formText = document.querySelector(".mobil__text");
 const letras = document.querySelector(".letras");
+const letrasUsadas = document.querySelector(".letras_pulsadas");
+const munheco = document.querySelectorAll("#munheco");
+const vida = document.querySelector(".vidas");
 
+// console.log(munheco);
+formText.value = "";
+vida.innerText = `Vidas: ${vidas}`;
+let i = 0;
+let cont = 0;
+let vic = 0;
+
+console.log(letras.childNodes);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  console.log(cont);
   let inputLetra = formText.value.toLowerCase();
-  if (write.includes(inputLetra)) {
-    // alert("Si es una letra que esta");
+  const div = document.createElement("div");
+  if (inputLetra === " " || inputLetra === "") {
+    alert("Tienes que escribir una letra o palabra");
+    return;
+  }
+
+  if (vidas === 0) {
+    alert("Has perdido");
+    return;
+  }
+
+  if (letra.includes(inputLetra)) {
     for (let i = 0; i < letras.childNodes.length; i++) {
-      // console.log(inputLetra, write[i]);
-      if (inputLetra === write[i]) {
-        console.log("Si");
-        // letras.childNodes[i];
-        console.log((letras.childNodes[i].innerText = inputLetra));
-      } else {
-        console.log("No");
+      if (inputLetra === letra[i]) {
+        letras.childNodes[i].innerText = inputLetra;
+        vic++;
+      }
+      if (vic === cont) {
+        alert("has ganado");
       }
     }
+  } else {
+    vida.innerText = `Vidas: ${(vidas = vidas - 1)}`;
   }
+  letrasUsadas.appendChild(div).classList = "letras__pulsadas-style";
+  letrasUsadas.childNodes[i].innerText = inputLetra;
+  i++;
+  formText.value = "";
 });
 
-for (let i = 0; i < write.length; i++) {
+if (letra === undefined) {
+  letra = "error";
+}
+
+for (let i = 0; i < letra.length; i++) {
   const div = document.createElement("div");
-  if (write[i] == " ") {
+  if (letra[i] == " ") {
     letras.appendChild(div).className = "space";
   } else {
     letras.appendChild(div).className = "letras_chart";
+    cont++;
+  }
+
+  if (letras.childNodes === 0) {
+    alert("No se ha cargado ningina palabra");
   }
 }
+
+const btnMenu = document.querySelector(".btn__menu");
+
+btnMenu.addEventListener("click", () => {
+  alert("Se a clickeado el menu");
+});
